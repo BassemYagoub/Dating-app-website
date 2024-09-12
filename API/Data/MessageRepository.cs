c440d9fad08566a46a9bc540f40130106164a59c
BassemYagoub <yagoub.bassem@gmail.com>
2024-09-12 11:35:02 +0200

    Section 18 : Unit of work pattern implemented + messages and edit pages enhanced

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -62,7 +62,7 @@ namespace API.Data {
         }
 
         public async Task<IEnumerable<MessageDTO>> GetMessageThread(string currentUsername, string recipientUsername) {
-            var messages = await context.Messages
+            var query = context.Messages
                 .Where(x =>
                     x.RecipientUsername == currentUsername
                         && x.RecipientDeleted == false
@@ -72,17 +72,16 @@ namespace API.Data {
                         && x.RecipientUsername == recipientUsername
                 )
                 .OrderBy(x => x.MessageSent)
-                .ProjectTo<MessageDTO>(mapper.ConfigurationProvider)
-                .ToListAsync();
+                .AsQueryable();
 
-            var unreadMessages = messages.Where(x => x.DateRead == null &&
+            var unreadMessages = query.Where(x => x.DateRead == null &&
                 x.RecipientUsername == currentUsername).ToList();
 
             if (unreadMessages.Count != 0) {
                 unreadMessages.ForEach(x => x.DateRead = DateTime.UtcNow);
             }
 
-            return messages;
+            return await query.ProjectTo<MessageDTO>(mapper.ConfigurationProvider).ToListAsync();
         }
 
         public void RemoveConnection(Connection connection) {
--- Client/src/app/_services/confirm.service.ts
+++ Client/src/app/_services/confirm.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
+import { ConfirmDialogComponent } from '../modals/confirm-dialog/confirm-dialog.component';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfirmService {
+  bsModalRef?: BsModalRef;
+  private modalService = inject(BsModalService);
 
-  constructor() { }
+  confirm(title = 'Confirmation',
+        message = 'Are you sure you want to do this?',
+        btnOkText = 'Yes',
+        btnCancelText = 'Cancel'
+  ): Observable<any> | undefined {
+
+    const config: ModalOptions = {
+      initialState: {
+        title,
+        message,
+        btnOkText,
+        btnCancelText
+      }
+    };
+    this.bsModalRef = this.modalService.show(ConfirmDialogComponent, config);
+    return this.bsModalRef.onHidden?.pipe(
+      map(() => {
+        if (this.bsModalRef?.content) {
+          return this.bsModalRef.content.result;
+        } else return false;
+      })
+    )
+  }
+
 }
